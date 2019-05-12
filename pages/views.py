1c832e078d0874049c47ebc004a694f64e49324c
Nadia <nadia.mohamed@solirius.com>
2019-05-12 17:07:00 +0100

    Add Welcome page

--- pages/views.py
+++ pages/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'pages/index.html', {})
 
-def about(request):
-    return render(request, 'pages/about.html', {})
+def welcome(request):
+    return render(request, 'pages/welcome.html', {})
