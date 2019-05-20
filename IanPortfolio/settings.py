98c8a0fd47f71fb38d50aab914f352d1a2eb589f
nadia <nadia.mohamed89@gmail.com>
2019-05-20 21:30:04 +0100

    Update settings.py

--- IanPortfolio/settings.py
+++ IanPortfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['https://desolate-thicket-87027.herokuapp.com']
 
 
 # Application definition
