3d5504f72cecf13ac04d5e721c2d47d14dd73fc7
nadia <nadia.mohamed89@gmail.com>
2019-05-21 11:00:23 +0100

    Add AWS setup

--- IanPortfolio/settings.py
+++ IanPortfolio/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages',
     'pages',
     'tearsheets',
 ]
@@ -133,5 +134,17 @@ STATICFILES_DIRS = [
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#AWS Settings
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = 'eu-west-2'
+
+AWS_S3_BUCKET_AUTH = False
+AWS_QUERYSTRING_AUTH = False
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
