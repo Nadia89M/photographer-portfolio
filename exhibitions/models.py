847295f5d2a822528e5ad351eb2e9ab44e2bc920
Nadia <nadia.mohamed89@gmail.com>
2019-06-18 16:49:22 +0100

    Adjustments

--- exhibitions/models.py
+++ exhibitions/models.py
@@ -7,6 +7,7 @@ class Exhibition(models.Model):
     photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
     date = models.DateTimeField(default=datetime.now, blank=True)
     is_published = models.BooleanField(default=True)
+    is_horizontal = models.BooleanField(default=True)
     published_date = models.DateTimeField(default=datetime.now, blank=True)
     def __str__(self):
         return self.title
