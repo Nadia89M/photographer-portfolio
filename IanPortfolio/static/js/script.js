4a8a0ef382d3618c4eb9b4c22af4a3a4a9488e14
Nadia <nadia.mohamed89@gmail.com>
2019-06-18 15:26:35 +0100

    Adjustments

--- IanPortfolio/static/js/script.js
+++ IanPortfolio/static/js/script.js
@@ -345,39 +345,3 @@ $('.square').hover(
     $(this).removeClass('flipping');
   }
 );
-
-/*Tearsheets*/
-var $imagesCarousel = $('.carouselOfImages').flickity({
-  // options
-  contain: true,
-  autoPlay: true,
-  wrapAround: true,
-  friction: 0.3
-});
-
-function resizeCells() {
-  var flkty = $imagesCarousel.data('flickity');
-  var $current = flkty.selectedIndex;
-  var $length = flkty.cells.length;
-  if ($length <= '5') {
-    $imagesCarousel.flickity('destroy');
-  }
-  $('.carouselOfImages .carouselImage').removeClass('nextToSelected');
-  $('.carouselOfImages .carouselImage')
-    .eq($current - 1)
-    .addClass('nextToSelected');
-  if ($current + 1 == $length) {
-    var $endCell = '0';
-  } else {
-    var $endCell = $current + 1;
-  }
-  $('.carouselOfImages .carouselImage')
-    .eq($endCell)
-    .addClass('nextToSelected');
-}
-
-resizeCells();
-
-$imagesCarousel.on('scroll.flickity', function() {
-  resizeCells();
-});
